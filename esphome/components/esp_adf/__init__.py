86eccf25acdb071125096cfbe57df0ced727405f
kiklhorn <6151364+kiklhorn@users.noreply.github.com>
2023-11-10 02:05:55 +0100

    test

--- esphome/components/esp_adf/__init__.py
+++ esphome/components/esp_adf/__init__.py
@@ -22,7 +22,7 @@ SUPPORTED_BOARDS = {
     "esp32s3box": "CONFIG_ESP32_S3_BOX_BOARD",
     "esp32s3boxlite": "CONFIG_ESP32_S3_BOX_LITE_BOARD",
     "esp32s3box3": "CONFIG_ESP32_S3_BOX_3_BOARD",
-    "esp32s3box2": "CONFIG_ESP32_S3_BOX_2_BOARD",  //just test
+    "esp32s3box2": "CONFIG_ESP32_S3_BOX_2_BOARD",  #just test
 }
 
 
